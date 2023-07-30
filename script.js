21a8615472de47c70e00c35faa7aae088d602a59
Joel <joelitoh@icloud.com>
2023-07-29 20:21:26 -0500

    playing with the pokemon api and JS

---
+++ script.js
@@ -0,0 +1,36 @@
+document.querySelector('#search').addEventListener("click",getPokemon);
+
+function lowerCaseName(string) {
+    return string.toLowerCase();
+  }
+
+function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getPokemon(e) {
+    const name = document.querySelector("#pokemonName").value;
+    const pokemonName = lowerCaseName(name);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => response.json())
+    .then((data) => {
+        document.querySelector(".pokemonBox").innerHTML = `
+        <div>
+        <img 
+        src="${data.sprites.other["official-artwork"].front_default}" 
+        alt="${capitalizeFirstLetter(data.name)}"
+        />
+    </div>
+    <div class="pokemonInfo">
+        <h1>${capitalizeFirstLetter(data.name)}</h1>
+        <p>Weight : ${data.weight}</p>
+    </div>
+        `;
+    })
+    .catch((err) => {
+        console.log('Pokemon Not Found', err);
+    });
+
+    e.preventDefault();
+}
